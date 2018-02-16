a20966e4eec208e14b9957442a09bce85b9e45be
Andy Jang <andyj@olympic.co.nz>
2018-02-16 15:36:28 +1300

    add past week id and some styling

--- ClubSandwich/App.xaml.cs
+++ ClubSandwich/App.xaml.cs
@@ -8,7 +8,7 @@ namespace ClubSandwich
         {
             InitializeComponent();
 
-            MainPage = new Login();
+            MainPage = new MembersPage();
         }
 
         protected override void OnStart()
---
+++ iOS/ImageCircleRenderer.iOS.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using ClubSandwich.iOS;
+using ClubSandwich.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ImageCircle), typeof(ImageCircleRenderer))]
+namespace ClubSandwich.iOS
+{
+    public class ImageCircleRenderer : ImageRenderer
+    {
+        public ImageCircleRenderer()
+        {
+        }
+
+        private void CreateCircle()
+        {
+            try
+            {
+                double min = Math.Min(Element.Width, Element.Height);
+                Control.Layer.CornerRadius = (float)(min / 2.0);
+                Control.Layer.MasksToBounds = false;
+                Control.Layer.BorderColor = Color.White.ToCGColor();
+                Control.Layer.BorderWidth = 3;
+                Control.ClipsToBounds = true;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+                return;
+
+            CreateCircle();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == VisualElement.HeightProperty.PropertyName ||
+                e.PropertyName == VisualElement.WidthProperty.PropertyName)
+            {
+                CreateCircle();
+            }
+        }
+    }
+}
