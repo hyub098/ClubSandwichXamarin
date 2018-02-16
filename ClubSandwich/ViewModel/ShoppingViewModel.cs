c9a3211c9e41043daa36e4c90a7d12a34565b275
Andrew Jeong <andrewj@olympic.co.nz>
2018-02-16 15:50:34 +1300

    Midway for shopping page

---
+++ ClubSandwich/ViewModel/ShoppingViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClubSandwich.Model;
+using ClubSandwich.Service;
+
+namespace ClubSandwich.ViewModel
+{
+    public class ShoppingViewModel : BaseViewModel
+    {
+        WeekService _weekService;
+
+        IList<Week> Weeks;
+        
+        public ShoppingViewModel()
+        {
+            Title = "Shopping";
+        }
+
+        public async void GetShoppingInfo()
+        {
+            _weekService = new WeekService();
+
+            var response = await _weekService.GetWeeks().ConfigureAwait(false);
+            Weeks = response.Data.Weeks.Select(x =>
+            {
+                x.Shopper.FullName = x.Shopper.FirstName + " " + x.Shopper.LastName;
+                return x;
+            }).ToList();
+
+            OnPropertyChanged("Weeks");
+        }
+    }
+}
